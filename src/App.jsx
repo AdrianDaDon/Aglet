3fcf5344ee1c47dc9aa0ca085c27479550735554
Adrian <80717684+AdrianDaDon@users.noreply.github.com>
2026-03-13 08:59:51 +0200

    playing around with scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import Header from './components/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div className="header-container">
+            <div className="site-logo">
+                <a href="./">
+                    <img className="site-logo-img" src="/Logo/aglet_logo.svg" alt="Aglet Logo" />
+                </a>
+            </div>
+
+            <nav className="main-nav">
+                <a href="./">home</a>
+                <a href="./contact">contact</a>
+            </nav>
+        </div>
+    </header>
+  )
+}
